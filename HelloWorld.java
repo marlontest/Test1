70d44157c5356e6fef7d30114b51b39a1cb482e6
Marlon Ou <omzmarlon@gmail.com>
2016-05-30 21:50:08 -0700

    branch1 first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.*;
 public class HelloWorld{
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+    Reader r = new Reader(scan.nextLine());
+    System.out.println(r.get());
   }
 }
---
+++ Reader.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Reader{
+private String s;
+
+public Reader(String s){
+  this.s = s+" is a reader";
+}
+
+public String get(){
+  return s;
+}
+}
